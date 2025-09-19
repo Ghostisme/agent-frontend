985b04d6b1edff0a98ba0d3b5309d9bd33fb771d
rich <rich@opsmateai.com>
2025-09-19 17:57:20 +0800

    :sparkles: feat: add access denied comp will route push login

--- apps/web/src/pages/AccessDenied.tsx
+++ apps/web/src/pages/AccessDenied.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import Button from "@nexus/ui/components/Button/index"
 export default function AccessDenied() {
+  const navigate = useNavigate();
+  navigate
   return (
     <div className="p-6">
       <span className="text-red-600">无权限访问</span>
-      <Button type='primary' color='primary' variant='primary'>登录</Button>
+      <Button type='primary' color='primary' variant='primary' onClick={() => navigate('/login')}>登录</Button>
       <Button variant="default" className="flex-1 sm:flex-none">
         主要操作
       </Button>
